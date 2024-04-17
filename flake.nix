49ad8121bcdce032521d9361678f3dbe1986246d
0xgleb <gleb.dianov@protonmail.com>
2024-04-17 20:31:02 +0200

    first draft

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Sia renterd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        renterd = with pkgs;
+          let
+            zipUrl = if stdenv.isDarwin then {
+              url =
+                "https://github.com/SiaFoundation/renterd/releases/download/v1.0.6/renterd_darwin_amd64.zip";
+              hash = "sha256-YTJcJC52ZNswUrVclcYMiEfJKfEoJ1/X7p6nrEkGAg0=";
+              stripRoot = false;
+            } else {
+              url =
+                "https://github.com/SiaFoundation/renterd/releases/download/v1.0.6/renterd_linux_amd64.zip";
+              hash = "sha256-PGDwmwhXu8d6ivZ4GWVyPK+Z4FrEDRjnVqQsK5vouDE=";
+              stripRoot = false;
+            };
+
+          in stdenv.mkDerivation {
+            name = "renterd";
+            src = fetchzip zipUrl;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -v renterd $out/bin
+            '';
+          };
+
+      in {
+        packages = { inherit renterd; };
+
+        renterd = {
+          type = "app";
+          program = "${renterd}/bin/renterd";
+        };
+
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [ ];
+          packages = [ renterd ];
+        };
+      });
+
+}
